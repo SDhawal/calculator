7b81edaffbed8c2f451aa95dad85a3cfc4e874f5
Dhawal Sharma <dhawalsharma26@icloud.com>
2022-09-16 20:01:27 -0400

    app setup and app.js done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import {  useState,useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+const [preState,setPreState] = useState("")
+const [currState,setCurrState] = useState("")
+const [input,setInput] = useState("0")
+const [operator ,setOperator] = useState(null)
+const [total,setTotal] = useState(false)
+
+const inputNumber = e =>{
+
+}
+
+const operatorType = e =>{
+
+}
+
+const equals = e =>{
+
+}
+const plusMinus = () =>{
+
+}
+const reset = () =>{
+
+}
+const percent = e =>{
+
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='wrapper'>
+      <div className="display-screen"></div>
+      <div className="grey-buttons" onClick={reset} >AC</div>
+      <div className="grey-buttons" onClick={plusMinus} >+/-</div>
+      <div className="grey-buttons" onClick={percent} >%</div>
+      <div className="orange-buttons" onClick={operatorType}>÷</div>
+      <div className="dark-grey-buttons" onClick={inputNumber}>7</div>
+      <div className="dark-grey-buttons" onClick={inputNumber}>8</div>
+      <div className="dark-grey-buttons" onClick={inputNumber}>9</div>
+      <div className="orange-buttons" onClick={operatorType}>X</div>
+      <div className="dark-grey-buttons" onClick={inputNumber}>4</div>
+      <div className="dark-grey-buttons" onClick={inputNumber}>5</div>
+      <div className="dark-grey-buttons" onClick={inputNumber}>6</div>
+      <div className="orange-buttons" onClick={operatorType}>-</div>
+      <div className="dark-grey-buttons" onClick={inputNumber}>1</div>
+      <div className="dark-grey-buttons" onClick={inputNumber}>2</div>
+      <div className="dark-grey-buttons" onClick={inputNumber}>3</div>
+      <div className="orange-buttons" onClick={operatorType}>+</div>
+      <div className="wide-dark-grey-button"onClick={inputNumber}>0</div>
+      <div className="dark-grey-buttons">.</div>
+      <div className="orange-buttons" onClick={equals}>=</div>
+
+
+
+      </div>
+     
     </div>
   );
 }
